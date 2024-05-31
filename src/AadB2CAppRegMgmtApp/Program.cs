743b8b6cd15acdc8803ad216d409b144d9b76e41
Moim HA <moimhossain@gmail.com>
2024-05-31 12:32:57 +0200

    added more changes

--- src/AadB2CAppRegMgmtApp/Program.cs
+++ src/AadB2CAppRegMgmtApp/Program.cs
@@ -13,13 +13,42 @@ var clientSecretCredential = new ClientSecretCredential(
 try
 {
     string[] scopes = { "https://graph.microsoft.com/.default" };
-
     AccessToken token = await clientSecretCredential.GetTokenAsync(new TokenRequestContext(scopes));
-
     Console.WriteLine($"Access Token: {token.Token}");
-
-
     var graphClient = new GraphServiceClient(clientSecretCredential);
+
+
+    //var requestBody = new SchemaExtension
+    //{
+    //    Id = "cloudoven_appmetadata",
+    //    Description = "custom extensions demo",
+    //    TargetTypes = ["Group"],
+    //    Properties = 
+    //    [
+    //        new ExtensionSchemaProperty
+    //        {
+    //            Name = "partnerId",
+    //            Type = "Integer",
+    //        },
+    //        new ExtensionSchemaProperty
+    //        {
+    //            Name = "partnerName",
+    //            Type = "String",
+    //        },
+    //        new ExtensionSchemaProperty
+    //        {
+    //            Name = "partnerType",
+    //            Type = "String",
+    //        }
+    //    ]
+    //};    
+    //var result = await graphClient.SchemaExtensions.PostAsync(requestBody);
+
+
+    //await RegisterAppAsync(graphClient);
+
+
+
     var appObjectId = "754fdcaa-7744-4b75-abab-f99aaca481bd";
 
     var application = await graphClient.Applications[appObjectId].GetAsync();
@@ -29,7 +58,7 @@ try
         Console.WriteLine($"Application found: {application.DisplayName}");
 
 
-        
+
 
 
         //await CreateClientSecretAsync(graphClient, appObjectId);
@@ -63,4 +92,29 @@ static async Task CreateClientSecretAsync(GraphServiceClient graphClient, string
         Console.WriteLine($"Secret ID: {passwordCredentialResponse.CustomKeyIdentifier}");
         Console.WriteLine($"Secret Value: {passwordCredentialResponse.SecretText}");
     }
+}
+
+static async Task RegisterAppAsync(GraphServiceClient graphClient)
+{
+    var newApplication = new Application
+    {
+        DisplayName = $"MOIMHOSSAIN-{DateTime.Now.Ticks}",
+        SignInAudience = "AzureADMyOrg",
+        Tags = ["Pizza", "Dominos"],
+        ExtensionProperties = new List<ExtensionProperty>
+        {
+            new ExtensionProperty
+            {
+                DataType = "String",
+                Name = "extension_foo",
+                TargetObjects = ["Application"]
+            }
+        },
+        Web = new WebApplication
+        {
+            RedirectUris = ["https://microsoft.com"]
+        }
+    };
+
+    var app = await graphClient.Applications.PostAsync(newApplication);
 }
